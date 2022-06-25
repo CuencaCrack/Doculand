3c49078bc9d5416840215ab1bdfa753ebef694c4
CuencaKill <cuencamarcos44@gmail.com>
2022-06-25 15:09:32 -0500

    Reestructuracion del proyecto desde el comienzo. Creacion del codigo HTML.

--- Doculand/javaScript/header.js
+++ Doculand/javaScript/header.js
@@ -2,9 +2,9 @@ $(document).ready(function(){
 
 	$(window).scroll(function(){
 		if( $(this).scrollTop() > 0 ){
-			$('header').addClass('header2');image.png
+			$('header').addClass('header2');
 		} else {
-			$('header').removeClass('header2');
+			/*$('header').removeClass('header2');*/
 		}
 	});
 
